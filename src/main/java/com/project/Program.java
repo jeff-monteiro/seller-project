0ccf350102a7e1d7d772a13cf25cf296734b5df9
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-11-10 01:19:23 -0300

    first_commit

---
+++ src/main/java/com/project/Program.java
@@ -0,0 +1,17 @@
+package com.project;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+import db.DbException;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        /*TODO*/
+
+    }
+}
