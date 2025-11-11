1b14ec7a69f22c56af0345d0e15560fb9e6897b2
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-11-11 00:04:23 -0300

    Seller entity class

--- src/main/java/com/project/application/Program.java
+++ src/main/java/com/project/application/Program.java
@@ -1,6 +1,11 @@
 package com.project.application;
 
 import com.project.model.entities.Department;
+import com.project.model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
 
@@ -10,6 +15,9 @@ public class Program {
         department.setId(123456);
         department.setName("Books");
 
+        Seller obj = new Seller(24, "Bob", "jeffmonteiro@icloud.com", new Date(), 7000.00, department);
+
         System.out.println("Esse é o id: " + department.getId() + " e esse é o nome do department: " + department.getName());
+        System.out.println(obj);
     }
 }
