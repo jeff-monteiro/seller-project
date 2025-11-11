85b7734e674441b0932185a7a794742290b4b3db
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2025-11-10 23:10:05 -0300

    creating department class

---
+++ src/main/java/com/project/application/Program.java
@@ -0,0 +1,15 @@
+package com.project.application;
+
+import com.project.model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department department = new Department();
+        department.setId(123456);
+        department.setName("Central de Pesquisa");
+
+        System.out.println("Esse é o id: " + department.getId() + " e esse é o nome do department: " + department.getName());
+    }
+}
